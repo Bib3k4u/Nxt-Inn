f36bbc24e5e96f99d6f7bce2b0caa3c906745f33
bibek zha <baba@jha2.com>
2023-01-03 22:46:32 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>NXT-INN</h1>
+        <p>Next Innovation</p>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/channel/UCZYRDuoucHs5OVP5SZ72fJw" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/_zha_bibek_/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Bib3k4u" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>NXT-INN</h1>
+          <p>Next Innovation</p>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in market.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            We are the undergraduate persuing B.Tech in Computer Science Engineering and
+            we are the one who have a lot of experience on Full Stack Web Development.
+            We have worked with many brands like Aidcom IT solutions, Maven Foundation and 
+            they were glad after looking to our work. We are dedicated and hardworking, and
+            We demonstrated these traits while taking night classes for a master's program
+            from last three years, and working part time as a full stack Developer.
+            During that time, we managed multiple commitments but delivered and performed
+            above our expectations. Now that we are on to our commitment and will fulfill your requirement and 
+            we promise to match your ethic to complete projects with a larger company.
+             
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
